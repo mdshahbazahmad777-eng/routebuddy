68e9e5557a46c16a7a0bb72cfb674cdfd9719759
mdshahbazahmad777-eng <mdshahbazahmad777@gmail.com>
2026-08-13 19:55:18 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'routebuddy-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch / Listen for network requests
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
